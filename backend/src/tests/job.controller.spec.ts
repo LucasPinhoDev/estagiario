bdc5e559c34d51bf422026871d0cfae6e02bdd96
PinhoBC <102967270+PinhoBC@users.noreply.github.com>
2023-07-03 20:15:25 -0300

    Update job.controller.spec.ts

--- backend/src/tests/job.controller.spec.ts
+++ backend/src/tests/job.controller.spec.ts
@@ -1,45 +1,167 @@
-import { Test } from '@nestjs/testing';
-import { JobService } from '../job/job.service';
-import { PrismaService } from '../prisma/prisma.service';
-import * as jwt from 'jsonwebtoken';
+import { Test, TestingModule } from '@nestjs/testing';
+import { JobController } from './job.controller';
+import { JobService } from './job.service';
 
-describe('JobService', () => {
-  let jobService: JobService;
-  let prismaService: PrismaService;
+describe('JobController', () => {
+  let controller: JobController;
+  let service: JobService;
 
   beforeEach(async () => {
-    const moduleRef = await Test.createTestingModule({
-      providers: [JobService, PrismaService],
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [JobController],
+      providers: [JobService],
     }).compile();
 
-    jobService = moduleRef.get<JobService>(JobService);
-    prismaService = moduleRef.get<PrismaService>(PrismaService);
+    controller = module.get<JobController>(JobController);
+    service = module.get<JobService>(JobService);
   });
 
-  describe('createJob', () => {
-    it('should create a new job', async () => {
-      // Simulated job data to be created
-      const jobData = {
-        title: 'Desenvolvedor Full Stack',
-        jobLocationType: 'Remoto',
-        desc: 'Descrição do trabalho',
-        desiredResponsibility: 'Responsabilidades desejadas',
-        necessaryKnowledge: 'Conhecimentos necessários',
-        benefits: 'Benefícios oferecidos',
-        value: 10000,
-        companyId: 'companyId123',
-        token: 'token123',
-      };
+  it('should create a job', async () => {
+    const jobData = {
+      title: 'Software Engineer',
+      jobLocationType: 'Remote',
+      desc: 'Job description',
+      desiredResponsibility: 'Responsibilities',
+      necessaryKnowledge: 'Requirements',
+      benefits: 'Benefits',
+      value: 5000,
+      applyLink: 'https://example.com/apply',
+      companyId: '123',
+      token: 'token',
+    };
 
-      // Mock the Prisma create method
-      jest.spyOn(prismaService.job, 'create').mockResolvedValue({} as any);
+    const createdJob = {
+      id: '1',
+      ...jobData,
+    };
 
-      const createdJob = await jobService.createJob(jobData);
+    jest.spyOn(service, 'createJob').mockResolvedValue(createdJob);
 
-      expect(prismaService.job.create).toHaveBeenCalledWith({
-        data: jobData,
-      });
-      expect(createdJob).toEqual({} as any);
-    });
+    const result = await controller.createJob(jobData);
+
+    expect(service.createJob).toHaveBeenCalledWith(jobData);
+    expect(result).toEqual(createdJob);
+  });
+
+  it('should find a job', async () => {
+    const jobData = {
+      token: 'token',
+      find: 'Software Engineer',
+    };
+
+    const foundJob = {
+      id: '1',
+      title: 'Software Engineer',
+      jobLocationType: 'Remote',
+      desc: 'Job description',
+      desiredResponsibility: 'Responsibilities',
+      necessaryKnowledge: 'Requirements',
+      benefits: 'Benefits',
+      value: 5000,
+      applyLink: 'https://example.com/apply',
+      companyId: '123',
+    };
+
+    jest.spyOn(service, 'findJob').mockResolvedValue(foundJob);
+
+    const result = await controller.findJob(jobData);
+
+    expect(service.findJob).toHaveBeenCalledWith(jobData);
+    expect(result).toEqual(foundJob);
+  });
+
+  it('should find all jobs', async () => {
+    const params = 'param1=value1&param2=value2';
+
+    const allJobs = [
+      {
+        id: '1',
+        title: 'Software Engineer',
+        jobLocationType: 'Remote',
+        desc: 'Job description',
+        desiredResponsibility: 'Responsibilities',
+        necessaryKnowledge: 'Requirements',
+        benefits: 'Benefits',
+        value: 5000,
+        applyLink: 'https://example.com/apply',
+        companyId: '123',
+      },
+      {
+        id: '2',
+        title: 'Frontend Developer',
+        jobLocationType: 'Office',
+        desc: 'Job description',
+        desiredResponsibility: 'Responsibilities',
+        necessaryKnowledge: 'Requirements',
+        benefits: 'Benefits',
+        value: 4000,
+        applyLink: 'https://example.com/apply',
+        companyId: '456',
+      },
+    ];
+
+    jest.spyOn(service, 'findAll').mockResolvedValue(allJobs);
+
+    const result = await controller.findAll(params);
+
+    expect(service.findAll).toHaveBeenCalledWith(params);
+    expect(result).toEqual(allJobs);
+  });
+
+  it('should find a job by ID', async () => {
+    const id = '1';
+
+    const foundJob = {
+      id: '1',
+      title: 'Software Engineer',
+      jobLocationType: 'Remote',
+      desc: 'Job description',
+      desiredResponsibility: 'Responsibilities',
+      necessaryKnowledge: 'Requirements',
+      benefits: 'Benefits',
+      value: 5000,
+      applyLink: 'https://example.com/apply',
+      companyId: '123',
+    };
+
+    jest.spyOn(service, 'findById').mockResolvedValue(foundJob);
+
+    const result = await controller.findById(id);
+
+    expect(service.findById).toHaveBeenCalledWith(id);
+    expect(result).toEqual(foundJob);
+  });
+
+  it('should update a job', async () => {
+    const jobData = {
+      id: '1',
+      title: 'Software Engineer',
+      jobLocationType: 'Remote',
+      desc: 'Job description',
+      desiredResponsibility: 'Responsibilities',
+      necessaryKnowledge: 'Requirements',
+      benefits: 'Benefits',
+      value: 5000,
+      applyLink: 'https://example.com/apply',
+      companyId: '123',
+    };
+
+    jest.spyOn(service, 'updateJob').mockResolvedValue(jobData);
+
+    const result = await controller.updateJob({ jobData });
+
+    expect(service.updateJob).toHaveBeenCalledWith({ jobData });
+    expect(result).toEqual(jobData);
+  });
+
+  it('should delete a job', async () => {
+    const jobId = '1';
+
+    jest.spyOn(service, 'deleteJob').mockResolvedValue(undefined);
+
+    const result = await controller.deleteJob({ jobId });
+
+    expect(service.deleteJob).toHaveBeenCalledWith({ jobId });
+    expect(result).toBeUndefined();
   });
 });
